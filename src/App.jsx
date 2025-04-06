fb4bbcf629ec0a67b646a8c6fd533c2d6f9cfd4a
DharmpalSinghSisodiya <dharmpalsinghsisodiya68@gmail.com>
2025-04-06 14:53:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Collaboration from "./Collaboration";
+import Benefits from "./components/Benefits";
+import Button from "./components/Button";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
+import Services from "./components/Services";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <>
+      <div
+        className="pt-[4.75rem] lg:pt-[5.25rem] 
+    overflow-hidden"
+      >
+        <Header />
+        <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
